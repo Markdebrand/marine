41f361ed97a73cf06205ace58413663f9e1233a5
G4bomon <gabrielmontesbrea@gmail.com>
2026-02-26 10:38:03 -0400

    cambio para que funcione el css

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  basePath: "/tracking",
+  assetPrefix: "/tracking",
 };
 
 export default nextConfig;
